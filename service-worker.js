9166451133325d70ee709577259b5e3c103bbdf3
efal <32618426+efal@users.noreply.github.com>
2025-11-17 13:43:33 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'lyric-teleprompter-v6'; // Version bumped to trigger update
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.svg',
-  '/icon-512.svg',
-  '/screenshot-desktop.svg',
-  '/screenshot-mobile.svg'
+  'index.html',
+  'manifest.json',
+  'icon-192.svg',
+  'icon-512.svg',
+  'screenshot-desktop.svg',
+  'screenshot-mobile.svg'
 ];
 
 // Install the service worker and cache the app shell
